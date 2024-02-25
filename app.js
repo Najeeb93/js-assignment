6bfed83ec35aaf4324c335881b0de8d7bf76f356
iamnajeeb <153761883+Najeeb93@users.noreply.github.com>
2024-02-25 20:47:45 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,78 @@
+
+//Assignment#1
+
+
+//Q1//
+alert("Hello User")
+
+//Q2//
+alert("Error!Please enter a valid password.")
+
+//Q3//
+alert("Welcome to JS Land...\n Happy Coding!")
+
+//Q4//
+alert("Welocme to JS Land...")
+
+alert("Happy coding!")
+
+//Q5//
+var a = "Hello... i can run JS through my web browser's console"
+console.log(a)
+
+//Assignment#2 variables for strings
+
+//Q1
+var userName = "Jhone Doe"
+var age = "15"
+
+alert(userName)
+
+//Q2
+var userName ="PIZZA \nPIZZ \nPIZ \nPI \nP"
+alert(userName)
+
+//Q3
+var email ="my email address is example@example.com"
+alert(email)
+
+//Q4
+var book ="I am trying to learn from the Book A smarter \nway to learn Javascript"
+alert(book)
+
+//Q5
+//var a ="Yah! I can write HTML content through Javascript"
+//document.write(a)
+
+//Q6
+var a ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(a)
+
+
+
+//Assignment#3 Variables of Number
+
+//Q1
+
+var a ="I am 15 years old"
+alert(a)
+
+//Q2 
+
+var a ="You have visited this site 14 times"
+alert(a)
+
+//Q3
+
+//var birthYear ="My birth year is 1990 \n Date type of my declared variable is number"
+//document.write(birthYear)
+
+//Q4 
+
+var clientName=("john doe")
+var num=5;
+var storeName=("xyzclothing")
+document.write(" "+clientName+" ordered "+num+" t-shirts(s) on "+storeName+" store.")
+
+
+
